22472e86de687188923f8152a7d0bcc7c07c1b0d
keiz1213 <fukuoka00tsc@gmail.com>
2021-10-21 13:25:41 +0900

    rubocopで修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :self_introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name postal_code address self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postal_code address self_introduction])
   end
 end
--- db/migrate/20211019213207_rename_addless_column_to_users.rb
+++ db/migrate/20211019213207_rename_addless_column_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameAddlessColumnToUsers < ActiveRecord::Migration[6.1]
   def change
     rename_column :users, :addless, :address
