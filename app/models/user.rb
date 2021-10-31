bf50edb4c8a15bfdefc456e7170b46d6536fdbf3
keiz1213 <fukuoka00tsc@gmail.com>
2021-10-31 09:11:32 +0900

    画像アップロード機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :image
+  validate :image_type
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def image_type
+    errors.add(:image, 'はpng、jpegまたはgifでアップロードしてください') unless image.blob.content_type.in?(%('image/png image/jpeg image/gif'))
+  end
 end
