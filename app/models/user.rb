ffb24c2a2799bf45f97a804d6aa47868fae6823d
keiz1213 <fukuoka00tsc@gmail.com>
2021-11-23 12:10:09 +0900

    unfollowメソッド修正
    1.find_by、destroyメソッドで処理に失敗した時に例外を出すようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   end
 
   def unfollow(other_user)
-    active_follows.find_by(followed_id: other_user.id).destroy
+    active_follows.find_by!(followed_id: other_user.id).destroy!
   end
 
   def following?(other_user)
